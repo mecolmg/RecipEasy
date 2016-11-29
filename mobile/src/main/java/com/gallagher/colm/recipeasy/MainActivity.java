837494252c209b14ff4c5c56d4bf6d7078b0bb09
mecolmg <mecolmg@vt.edu>
2016-11-28 20:38:18 -0500

    Initial commit

---
+++ mobile/src/main/java/com/gallagher/colm/recipeasy/MainActivity.java
@@ -0,0 +1,39 @@
+package com.gallagher.colm.recipeasy;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
